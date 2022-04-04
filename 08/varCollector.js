1e33f2fb0d1cc7be578d5ba2ccbf64b96177846b
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2022-04-04 20:54:20 +0430

    downloader function implemented

--- 08/varCollector.js
+++ 08/varCollector.js
@@ -1,3 +1,9 @@
+// Storing the download button
+const dlButton = document.querySelector('.getCSSFile');
+
+// This is going to use later in the "Button's click event handler"
+let dataString = '';
+
 // A recursive function which generates an object literal out of an Array
 function arrayToObject(arr) {
   return Object.fromEntries(
@@ -6,7 +12,6 @@ function arrayToObject(arr) {
     )
   );
 }
-
 
 function getCSSCustomProp(selector = null) {
   // Collecting all "CSSStyleRule" for each "selector" from provided styleSheet locally
@@ -40,12 +45,12 @@ function getCSSCustomProp(selector = null) {
   const cssRulesObj = arrayToObject(cssRulesArray);
 
   //! 👇🏻 IMPORTANT BUG FIXED 👇🏻 //
-  // 
+  //
   Object.keys(cssRulesObj).forEach((key) => {
     for (const item in cssRulesObj[key]) {
-      cssRulesObj[key][item] = getComputedStyle(
-        document.querySelector(key)
-      ).getPropertyValue(item).trim();
+      cssRulesObj[key][item] = getComputedStyle(document.querySelector(key))
+        .getPropertyValue(item)
+        .replace(/"|'| |;|\\/gi, '');
     }
   });
   //! 👆🏻 IMPORTANT BUG FIXED 👆🏻 //
@@ -61,12 +66,16 @@ document.querySelector('.var-logger').addEventListener('click', (e) => {
   e.preventDefault();
 
   if (inputBox.disabled) {
+    dataString = stringifyData(getCSSCustomProp());
+    dlButton.classList.remove('hidden');
     return console.log(getCSSCustomProp());
   }
 
   if (inputBox.value === '') {
     return console.warn(`InputBox is empty!`);
   } else {
+    dataString = stringifyData(getCSSCustomProp(inputBox.value));
+    dlButton.classList.remove('hidden');
     return console.log(getCSSCustomProp(inputBox.value));
   }
 });
@@ -78,3 +87,38 @@ document.querySelectorAll('.the-form [type=radio]').forEach((element) =>
     inputBox.value = '';
   })
 );
+
+/*---------------------------------------------*/
+/* Downloading the final object as a .css file */
+/*---------------------------------------------*/
+
+// Converts the CSSCustomProp object to proper string in order to make it CSSify
+function stringifyData(obj) {
+  return JSON.stringify(obj)
+    .replace(/^{/g, '')
+    .replace(/(?<="}),(?=")/g, '%n%\n')
+    .split('%n%')
+    .map((item) =>
+      item
+        .replace(/:{/gi, '{')
+        .replace(/ |"|'/gi, '')
+        .replace(/(?<=\S){(?=\S)/gi, ' {\n')
+        .replace(/(?<=\S)}/gi, ';\n}\n')
+        .replace(/,/gi, ';\n')
+        .replace(/(?<=\S):/gi, ': ')
+        .replace(/--(?=\S+:)/gi, '  --')
+        .replace(/}\n;\n}/gi, '}')
+    )
+    .join('');
+}
+
+function downloader(data, type, name) {
+  let blob = new Blob([data], { type });
+  dlButton.href = window.URL.createObjectURL(blob);
+  dlButton.download = name;
+}
+
+dlButton.addEventListener('click', (e) => {
+  downloader(dataString, 'text/css', 'style.css');
+  dlButton.classList.add('hidden');
+});
